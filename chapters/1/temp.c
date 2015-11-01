84b34405d652eb41ff71a909a45b4cd43fff7d52
Justin Force <justin.force@gmail.com>
2015-10-31 20:23:11 -0700

    Put conversion formulae in constants

--- chapters/1/temp.c
+++ chapters/1/temp.c
@@ -4,26 +4,26 @@
 #define UPPER 300 /* upper limite of table */
 #define STEP  20  /* step size */
 
+/* Formulae to convert Fahrenheit to Celcius and vice versa */
+#define F_TO_C(F) (5.0/9.0) * (F - 32.0)
+#define C_TO_F(C) ((C / (5.0/9.0)) + 32.0)
+
 int main()
 {
-    float celcius, fahrenheit;
+    float degrees;
 
     printf("Fahrenheit | Celcius\n");
     printf("--------------------\n");
-    for (fahrenheit = UPPER; fahrenheit >= LOWER; fahrenheit -= STEP) {
-        celcius = (5.0/9.0) * (fahrenheit - 32.0);
-        printf("%10.f | %6.1f\n", fahrenheit, celcius);
-    }
+    for (degrees = UPPER; degrees >= LOWER; degrees -= STEP)
+        printf("%10.f | %6.1f\n", degrees, F_TO_C(degrees));
     printf("--------------------\n");
 
     printf("\n");
 
     printf("Celcius | Fahrenheit\n");
     printf("--------------------\n");
-    for (celcius = UPPER; celcius >= LOWER; celcius -= STEP) {
-        fahrenheit = (celcius / (5.0/9.0)) + 32.0;
-        printf("%7.f | %10.1f\n", celcius, fahrenheit);
-    }
+    for (degrees = UPPER; degrees >= LOWER; degrees -= STEP)
+        printf("%10.f | %6.1f\n", degrees, C_TO_F(degrees));
     printf("--------------------\n");
 
     return 0;
